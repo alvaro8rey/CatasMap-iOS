01cb03acf45027534e33a6e1318deb4800cbc409
Alvaro Rey <alvarorey7@gmail.com>
2026-04-23 22:01:29 +0200

    Initial Commit

---
+++ CatasMap/CatasMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatasMapApp.swift
+//  CatasMap
+//
+//  Created by alvaro on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct CatasMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatasMapTests/CatasMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatasMapTests.swift
+//  CatasMapTests
+//
+//  Created by alvaro on 23/04/2026.
+//
+
+import Testing
+@testable import CatasMap
+
+struct CatasMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
